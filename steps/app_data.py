0effe93e2882090ada8c5a58a53a9506affad396
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2024-01-06 13:15:27 -0300

    chore: remove stuff

--- steps/app_data.py
+++ steps/app_data.py
@@ -1,8 +1,19 @@
 from scapy.all import Packet, Raw
 
+"""
+  Esse passo anonimiza os dados da camada de aplicação.
+  De acordo com o que foi definido no trabalho de pesquisa,
+  os dados da camada de aplicação serão substituídos por 
+  uma sequência de bytes nulos (0x00) utilizando o algortimo de black marker.
+
+  Args:
+    packet: pacote a ser anonimizado
+  Output:
+    packet: pacote com os dados da camada de aplicação anonimizados
+"""
 def anon_app_data(packet: Packet) -> Packet:
     if Raw in packet:
       payload_length = len(packet[Raw].load)
-      packet[Raw].load = b"\x00" * payload_length  
+      packet[Raw].load = b"\x00"
     return packet
  
--- steps/checksum.py
+++ steps/checksum.py
@@ -1,6 +1,10 @@
 
 from scapy.all import Packet, IP, TCP
 
+
+"""
+  Esse passo recalcula os checksums dos pacotes.
+"""
 def recalculate(packet: Packet) -> Packet:
     if IP in packet:
       del packet[IP].chksum
