943fc94720256702e6ddacbe3ff8b41bbda06d1b
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2024-05-13 22:14:32 -0300

    fix(WIP): remove len check

--- steps/checksum.py
+++ steps/checksum.py
@@ -17,12 +17,18 @@ def recalculate(packet: Packet, index: int) -> Packet:
     try:
         if packet.haslayer(IP):
             del packet[IP].chksum
-            actual_len = len(packet[IP])
-            if actual_len != packet[IP].len:
-                logging.warning(
-                    f"Incorrect IP length for packet {index}, probably truncated, recalculating length"
-                )
-                del packet[IP].len
+            # actual_len = len(packet[IP])
+            # if actual_len != packet[IP].len:
+            #     print(
+            #         f"packet {index} has actual_len {actual_len} != packet[IP].len {packet[IP].len}"
+            #     )
+            #     print(packet[IP])
+            #     packet.summary()
+
+            #     logging.warning(
+            #         f"Incorrect IP length for packet {index}, probably truncated, recalculating length"
+            #     )
+            #     del packet[IP].len
 
         if packet.haslayer(TCP):
             del packet[TCP].chksum
@@ -35,12 +41,12 @@ def recalculate(packet: Packet, index: int) -> Packet:
 
         if packet.haslayer(UDP):
             del packet[UDP].chksum  # pacotes UDP podem ter checksums
-            actual_len = len(packet[UDP])
-            if actual_len != packet[UDP].len:
-                logging.warning(
-                    f"Incorrect UDP length for packet {index}, probably truncated, recalculating length"
-                )
-                del packet[UDP].len
+            # actual_len = len(packet[UDP])
+            # if actual_len != packet[UDP].len:
+            #     logging.warning(
+            #         f"Incorrect UDP length for packet {index}, probably truncated, recalculating length"
+            #     )
+            #     del packet[UDP].len
 
         return packet
     except Exception as e:
