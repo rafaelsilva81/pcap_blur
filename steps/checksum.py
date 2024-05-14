6f9453c5f8de3aa45db579cea2c82fbd556767ad
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2024-05-14 19:56:26 -0300

    refactor: update checksum.py

--- steps/checksum.py
+++ steps/checksum.py
@@ -17,12 +17,6 @@ def recalculate(packet: Packet, index: int) -> Packet:
     try:
         if packet.haslayer(IP):
             del packet[IP].chksum
-            actual_len = len(packet[IP])
-            if actual_len != packet[IP].len:
-                logging.warning(
-                    f"Incorrect IP length for packet {index}, probably truncated, recalculating length"
-                )
-                del packet[IP].len
 
         if packet.haslayer(TCP):
             del packet[TCP].chksum
@@ -34,13 +28,7 @@ def recalculate(packet: Packet, index: int) -> Packet:
             del packet[IGMPv3].chksum
 
         if packet.haslayer(UDP):
-            del packet[UDP].chksum  # pacotes UDP podem ter checksums
-            actual_len = len(packet[UDP])
-            if actual_len != packet[UDP].len:
-                logging.warning(
-                    f"Incorrect UDP length for packet {index}, probably truncated, recalculating length"
-                )
-                del packet[UDP].len
+            del packet[UDP].chksum
 
         return packet
     except Exception as e:
